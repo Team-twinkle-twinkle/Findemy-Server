ea622276e0a6179df8b29e39bf4d23e8e1b12692
dkflfkd53 <b57062562@gmail.com>
2025-07-15 14:52:51 +0900

    add :: User entity 추가

---
+++ src/main/java/com/founderz/findemy/domain/user/entity/User.java
@@ -0,0 +1,30 @@
+package com.founderz.findemy.domain.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "tbl_user")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String accountId;
+
+    @Column(nullable = false)
+    private String password;
+}
