5ea33087037d1f32226768eb8a315fdc3f405f83
xo-rxs <taekyun0525@gmail.com>
2025-07-15 17:15:27 +0900

    enum추가

---
+++ src/main/java/com/founderz/findemy/domain/academy/entity/Academy.java
@@ -0,0 +1,33 @@
+package com.founderz.findemy.domain.academy.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "tbl_academy")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Academy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String accountId;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String academyName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "sido", nullable = false)
+    private SiDo sido;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "sigungu", nullable = false)
+    private SiGunGu sigungu;
+}
