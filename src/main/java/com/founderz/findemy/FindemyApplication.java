cbb821d23fa08c0affc01fadd47d781020d99058
dkflfkd53 <b57062526@gmail.com>
2025-07-15 09:36:44 +0900

    first commit

---
+++ src/main/java/com/founderz/findemy/FindemyApplication.java
@@ -0,0 +1,13 @@
+package com.founderz.findemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindemyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindemyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/founderz/findemy/FindemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.founderz.findemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindemyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
